6e98fef0c069fab173913b023c62d30d04798302
coltonsr77 <coltonr3@icloud.com>
2026-03-27 11:26:04 -0400

    Update ValidateAccountInfo.js

--- zefie_wtvp_minisrv/includes/ServiceVault/wtv-register/ValidateAccountInfo.js
+++ zefie_wtvp_minisrv/includes/ServiceVault/wtv-register/ValidateAccountInfo.js
@@ -15,8 +15,6 @@ if (!request_headers.query.registering) {
     else if (request_headers.query.subscriber_username.length > minisrv_config.config.user_accounts.max_username_length) errpage = wtvshared.doErrorPage(400, "Please choose a username with <b>" + minisrv_config.config.user_accounts.max_username_length + "</b> or less characters.");
     else if (!wtvr.checkUsernameSanity(request_headers.query.subscriber_username)) errpage = wtvshared.doErrorPage(400, "The username you have chosen contains invalid characters. Please choose a username with only <b>letters</b>, <b>numbers</b>, <b>_</b> or <b>-</b>. Also, please be sure your username begins with a letter.");
     else if (!wtvr.checkUsernameAvailable(request_headers.query.subscriber_username)) errpage = wtvshared.doErrorPage(400, "The username you have selected is not available. Please select another username.");
-    else if (!request_headers.query.subscriber_contact) errpage = wtvshared.doErrorPage(400, "Please enter your contact information.");
-    else if (request_headers.query.subscriber_contact_method === "") errpage = wtvshared.doErrorPage(400, "Please select the type of contact information you provided.");
 
 
     if (errpage) {
@@ -37,8 +35,6 @@ Content-Type: text/html`;
 <input type=hidden name=registering value="true">
 <input type=hidden name=subscriber_name value="${request_headers.query.subscriber_name}">
 <input type=hidden name=subscriber_username value="${request_headers.query.subscriber_username}">
-<input type=hidden name=subscriber_contact value="${request_headers.query.subscriber_contact}">
-<input type=hidden name=subscriber_contact_method value="${request_headers.query.subscriber_contact_method}">
 <td height=50 width=300 colspan=6 valign=top align=left>
  &nbsp; <br>
 Here is your account information. If you need to<br>
@@ -52,11 +48,6 @@ correct an item, press <b>Back</b>.<p>`;
 <tt><font color=#d1d3d3 size=-2><spacer type=horizontal size=17> ${request_headers.query.subscriber_name}</font></tt></a>
 </table>
 <p>
-<table cellspacing=0 cellpadding=0 border=0> 
-<img src="images/arrow.gif">&nbsp;&nbsp;<font size=-2><b>CONTACT</b></font><br>
-<tt><font color=#d1d3d3 size=-2><spacer type=horizontal size=17> ${request_headers.query.subscriber_contact}</font></tt></a>
-</table>
-</TD>
 <td abswidth=200 valign=top align=left>
 <table cellspacing=0 cellpadding=0 border=0> <TR><TD>
 <img src="images/arrow.gif"><font size=-2>&nbsp;&nbsp;<b>USERNAME</b></font><br>
@@ -64,11 +55,6 @@ correct an item, press <b>Back</b>.<p>`;
 <tt><font color=#d1d3d3><spacer type=horizontal size=17>${request_headers.query.subscriber_username}</font></tt></a>
 </table>
 <p>
-<table cellspacing=0 cellpadding=0 border=0>
-	<img src="images/arrow.gif">&nbsp;&nbsp;<font size=-2><b>CONTACT TYPE</b></font><br>
-<tt><font color=#d1d3d3 size=-2><spacer type=horizontal size=17>${request_headers.query.subscriber_contact_method}</font></tt>
-</table> <P>&nbsp;<P>&nbsp;
-<td abswidth=20>
 </tr>`;
         if (isOldBuild) main_data += '</table>';
         const form_data = `
