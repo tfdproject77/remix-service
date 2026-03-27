61988c063cbb99fc47acabee0bcf87b486c98dd0
coltonsr77 <coltonr3@icloud.com>
2026-03-27 11:42:11 -0400

    Update home.js

--- zefie_wtvp_minisrv/includes/ServiceVault/wtv-home/home.js
+++ zefie_wtvp_minisrv/includes/ServiceVault/wtv-home/home.js
@@ -262,7 +262,7 @@ data += `
 <tr>
 <td valign="top" colspan=4 absheight=80 >
 &#149;&nbsp; Updated cSetup to 3.0-WTVR<br>
-&#149;&nbsp; Updated register<br>
+&#149;&nbsp; Updated Credits<br>
 &#149;&nbsp; Updated "Source" to show the WTVR website<br>
 &#149;&nbsp; Added a new WTV trick<br>
 </table>
